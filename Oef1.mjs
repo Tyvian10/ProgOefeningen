26a63036aedacbe26732f02a95206bfd7bec39c7
Tyvian10 <binanaapuassa@outlook.com>
2024-09-24 11:34:13 +0200

    Oefening 1 bijna klaar

---
+++ Oef1.mjs
@@ -0,0 +1,10 @@
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+const userInput = readline.createInterface({input, output});
+// Voeg hier je eigen code in
+
+let name = "tyvian"
+
+console.log('Hallo '+ name +', ik hoor dat je '+ age +' jaar oud bent en dat je lievelingsgame '+ favoriteGame +' is.');
+
+process.exit();
